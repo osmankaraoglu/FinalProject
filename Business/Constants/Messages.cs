1a12926f1b17ed14333241ad03513dfc7f20c3d7
0sman KARAOĞLU <osmnkaraoglu@gmail.com>
2022-05-26 13:26:48 +0300

    Hafta 14

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +18,13 @@ namespace Business.Constants
 
         public static string ProductNameAlreadyExists = "böyle bi isimde ürün zaten var.";
         public static string CheckIfCategoryLimitExceded = "Kategori limiti aşıldığı için yrni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+
+        public static string UserRegistered = "Kullanıcı kaydedildi";
+        public static string UserNotFound = "Kullanıcı Bulunamadı";
+        public static string PasswordError = "Parola hatalı";
+        public static string SuccessfulLogin = "Giriş Başarılı";
+        public static string UserAlreadyExists = "Kullanıcı zaten mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu";
     }
 }
